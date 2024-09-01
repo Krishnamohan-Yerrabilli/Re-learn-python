375d738c7a27d3fef37705c30dc86cfda240bb0f
Krishnamohan-Yerrabilli <krishnamohan72452964@gmail.com>
2024-09-01 11:31:02 +0530

    renamed the folder in ascending order

---
+++ 3.Operations/arth_op.py
@@ -0,0 +1,19 @@
+addition = 24 + 5
+
+mul = 33 * 2
+
+sub = 45 - 3
+
+division = 12 / 3
+
+modulus = 12 % 3
+
+print("add_result of 24+5:", addition)
+
+print("sub result of 45-3:", sub)
+
+print("mul result of 33*2:", mul)
+
+print("division result of 12/3:", division)
+
+print("modulus result of 12%3: ", modulus)
