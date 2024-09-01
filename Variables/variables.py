2b79abb12e8351191baf4e9967066e352c1c13db
Krishnamohan-Yerrabilli <krishnamohan72452964@gmail.com>
2024-09-01 11:02:15 +0530

    pushing hello-world into the newly created file, everything wants to be in organized format

---
+++ Variables/variables.py
@@ -0,0 +1,20 @@
+#integer variable
+
+age = 25
+
+#float variable
+
+temperature = 45.3
+
+#string variable
+name = "Alice"
+
+#Boolean variable
+Is_student = True 
+
+#print all the variable
+
+print("age: ", age)
+print("temperature: ", temperature)
+print("String of characters: ", name)
+print("Is_student_has_been_passed? : ", Is_student)
